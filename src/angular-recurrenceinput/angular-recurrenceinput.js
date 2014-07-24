696f3517192d0d95d5a0d3c1bf892b3833e14fb9
osipov <my_user_name@mail_from_google.com>
2014-07-24 15:32:15 -0400

    enabled custom i18n settings to be passed via ri-locale attribute on the directive

--- src/angular-recurrenceinput/angular-recurrenceinput.js
+++ src/angular-recurrenceinput/angular-recurrenceinput.js
@@ -5,7 +5,7 @@
         var LABELS = {};
         var tool = {
             conf: {
-                lang: 'en',
+                lang: defaultLocale,
                 readOnly: false,
                 firstDay: 0,
 
@@ -88,114 +88,10 @@
             }
         };
 
-        tool.localize("en", {
-            displayUnactivate: 'Repeat...',
-            displayActivate: 'Repeats every',
-            add_rules: 'Add',
-            edit_rules: 'Edit...',
-            delete_rules: 'Delete',
-            add: 'Add',
-            refresh: 'Refresh',
-
-            title: 'Repeat',
-            preview: 'Selected dates',
-            addDate: 'Add date',
-
-            recurrenceType: 'Repeats:',
-
-            dailyInterval1: 'Repeat every:',
-            dailyInterval2: 'days',
-
-            weeklyInterval1: 'Repeat every:',
-            weeklyInterval2: 'week(s)',
-            weeklyWeekdays: 'Repeat on:',
-            weeklyWeekdaysHuman: 'on:',
-
-            monthlyInterval1: 'Repeat every:',
-            monthlyInterval2: 'month(s)',
-            monthlyDayOfMonth1: 'Day',
-            monthlyDayOfMonth1Human: 'on day',
-            monthlyDayOfMonth2: 'of the month',
-            monthlyDayOfMonth3: 'month(s)',
-            monthlyWeekdayOfMonth1: 'The',
-            monthlyWeekdayOfMonth1Human: 'on the',
-            monthlyWeekdayOfMonth2: '',
-            monthlyWeekdayOfMonth3: 'of the month',
-            monthlyRepeatOn: 'Repeat on:',
-
-            yearlyInterval1: 'Repeat every:',
-            yearlyInterval2: 'year(s)',
-            yearlyDayOfMonth1: 'Every',
-            yearlyDayOfMonth1Human: 'on',
-            yearlyDayOfMonth2: '',
-            yearlyDayOfMonth3: '',
-            yearlyWeekdayOfMonth1: 'The',
-            yearlyWeekdayOfMonth1Human: 'on the',
-            yearlyWeekdayOfMonth2: '',
-            yearlyWeekdayOfMonth3: 'of',
-            yearlyWeekdayOfMonth4: '',
-            yearlyRepeatOn: 'Repeat on:',
-
-            range: 'End recurrence:',
-            rangeNoEnd: 'Never',
-            rangeByOccurrences1: 'After',
-            rangeByOccurrences1Human: 'ends after',
-            rangeByOccurrences2: 'occurrence(s)',
-            rangeByEndDate: 'On',
-            rangeByEndDateHuman: 'until',
-
-            including: ', and also',
-            except: ', except for',
-
-            cancel: 'Cancel',
-            save: 'Save',
-
-            recurrenceStart: 'Start of the recurrence',
-            additionalDate: 'Additional date',
-            include: 'Include',
-            exclude: 'Exclude',
-            remove: 'Remove',
-
-            orderIndexes: ['first', 'second', 'third', 'fourth', 'last'],
-            months: [
-                'January', 'February', 'March', 'April', 'May', 'June',
-                'July', 'August', 'September', 'October', 'November', 'December'],
-            shortMonths: [
-                'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            weekdays: [
-                'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-                'Friday', 'Saturday'],
-            shortWeekdays: [
-                'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-
-            longDateFormat: 'mmmm dd, yyyy',
-            shortDateFormat: 'mm/dd/yyyy',
-
-            unsupportedFeatures: 'Warning: This event uses recurrence features not ' +
-                                  'supported by this widget. Saving the recurrence ' +
-                                  'may change the recurrence in unintended ways:',
-            noTemplateMatch: 'No matching recurrence template',
-            multipleDayOfMonth: 'This widget does not support multiple days in monthly or yearly recurrence',
-            bysetpos: 'BYSETPOS is not supported',
-            noRule: 'No RRULE in RRULE data',
-            noRepeatEvery: 'Error: The "Repeat every"-field must be between 1 and 1000',
-            noEndDate: 'Error: End date is not set. Please set a correct value',
-            noRepeatOn: 'Error: "Repeat on"-value must be selected',
-            noEndRecurrence: 'Error: "End recurrence"-value must be selected',
-            pastEndDate: 'Error: End date cannot be before start date',
-            noEndAfterNOccurrences: 'Error: The "After N occurrences"-field must be between 1 and 1000',
-            alreadyAdded: 'This date was already added',
-
-            rtemplate: {
-                daily: 'Daily',
-                mondayfriday: 'Monday and Friday',
-                weekdays: 'Weekday',
-                weekly: 'Weekly',
-                monthly: 'Monthly',
-                yearly: 'Yearly'
-            }
-        });
+        //move all declared i18n locales to LABELS
+		for (var localeName in locale) {
+			tool.localize(localeName, locale[localeName]);
+		}
 
         /**********************************
             Directive definition
@@ -769,6 +665,12 @@
 			/**********************************
 				Control Setup
 			***********************************/
+			//move any i18n settings from riLocale to LABELS, overwriting the default ones
+			if ($scope.riLocale) {
+				for (var localeName in $scope.riLocale) 
+					tool.localize(localeName, $scope.riLocale[localeName]);
+			}
+						
 			var conf = angular.extend({}, tool.conf);
 			angular.extend(conf, $scope.riConfig);
 			angular.extend(conf, { i18n: LABELS[conf.lang], name: $scope.riName });
@@ -1190,6 +1092,7 @@
                 riName: '=',
                 EXDATES: '=riExdates',
                 RDATES: '=riRdates',
+                riLocale: '=',
                 riStartDate: '='
             },
             link: function (scope, elem, attr, ctrl) {
