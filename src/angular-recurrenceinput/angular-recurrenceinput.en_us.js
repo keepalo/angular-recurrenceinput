c94a949f0ed322ed33f617a5fd75c9e19d56bc9a
osipov <my_user_name@mail_from_google.com>
2014-07-23 23:30:26 -0400

    first step towards better i18n

---
+++ src/angular-recurrenceinput/angular-recurrenceinput.en_us.js
@@ -0,0 +1,107 @@
+locale[defaultLocale] = {
+    displayUnactivate: 'Repeat...',
+    displayActivate: 'Repeats every',
+    add_rules: 'Add',
+    edit_rules: 'Edit...',
+    delete_rules: 'Delete',
+    add: 'Add',
+    refresh: 'Refresh',
+
+    title: 'Repeat',
+    preview: 'Selected dates',
+    addDate: 'Add date',
+
+    recurrenceType: 'Repeats:',
+
+    dailyInterval1: 'Repeat every:',
+    dailyInterval2: 'days',
+
+    weeklyInterval1: 'Repeat every:',
+    weeklyInterval2: 'week(s)',
+    weeklyWeekdays: 'Repeat on:',
+    weeklyWeekdaysHuman: 'on:',
+
+    monthlyInterval1: 'Repeat every:',
+    monthlyInterval2: 'month(s)',
+    monthlyDayOfMonth1: 'Day',
+    monthlyDayOfMonth1Human: 'on day',
+    monthlyDayOfMonth2: 'of the month',
+    monthlyDayOfMonth3: 'month(s)',
+    monthlyWeekdayOfMonth1: 'The',
+    monthlyWeekdayOfMonth1Human: 'on the',
+    monthlyWeekdayOfMonth2: '',
+    monthlyWeekdayOfMonth3: 'of the month',
+    monthlyRepeatOn: 'Repeat on:',
+
+    yearlyInterval1: 'Repeat every:',
+    yearlyInterval2: 'year(s)',
+    yearlyDayOfMonth1: 'Every',
+    yearlyDayOfMonth1Human: 'on',
+    yearlyDayOfMonth2: '',
+    yearlyDayOfMonth3: '',
+    yearlyWeekdayOfMonth1: 'The',
+    yearlyWeekdayOfMonth1Human: 'on the',
+    yearlyWeekdayOfMonth2: '',
+    yearlyWeekdayOfMonth3: 'of',
+    yearlyWeekdayOfMonth4: '',
+    yearlyRepeatOn: 'Repeat on:',
+
+    range: 'End recurrence:',
+    rangeNoEnd: 'Never',
+    rangeByOccurrences1: 'After',
+    rangeByOccurrences1Human: 'ends after',
+    rangeByOccurrences2: 'occurrence(s)',
+    rangeByEndDate: 'On',
+    rangeByEndDateHuman: 'until',
+
+    including: ', and also',
+    except: ', except for',
+
+    cancel: 'Cancel',
+    save: 'Save',
+
+    recurrenceStart: 'Start of the recurrence',
+    additionalDate: 'Additional date',
+    include: 'Include',
+    exclude: 'Exclude',
+    remove: 'Remove',
+
+    orderIndexes: ['first', 'second', 'third', 'fourth', 'last'],
+    months: [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'],
+    shortMonths: [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    weekdays: [
+        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+        'Friday', 'Saturday'],
+    shortWeekdays: [
+        'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+
+    longDateFormat: 'mmmm dd, yyyy',
+    shortDateFormat: 'mm/dd/yyyy',
+
+    unsupportedFeatures: 'Warning: This event uses recurrence features not ' +
+                          'supported by this widget. Saving the recurrence ' +
+                          'may change the recurrence in unintended ways:',
+    noTemplateMatch: 'No matching recurrence template',
+    multipleDayOfMonth: 'This widget does not support multiple days in monthly or yearly recurrence',
+    bysetpos: 'BYSETPOS is not supported',
+    noRule: 'No RRULE in RRULE data',
+    noRepeatEvery: 'Error: The "Repeat every"-field must be between 1 and 1000',
+    noEndDate: 'Error: End date is not set. Please set a correct value',
+    noRepeatOn: 'Error: "Repeat on"-value must be selected',
+    noEndRecurrence: 'Error: "End recurrence"-value must be selected',
+    pastEndDate: 'Error: End date cannot be before start date',
+    noEndAfterNOccurrences: 'Error: The "After N occurrences"-field must be between 1 and 1000',
+    alreadyAdded: 'This date was already added',
+    rtemplate: {
+        daily: 'Daily',
+        mondayfriday: 'Monday and Friday',
+        weekdays: 'Weekday',
+        weekly: 'Weekly',
+        monthly: 'Monthly',
+        yearly: 'Yearly'
+    }
+};
